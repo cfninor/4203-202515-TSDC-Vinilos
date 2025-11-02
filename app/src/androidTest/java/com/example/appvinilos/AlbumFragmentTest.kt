efab6a482b24869c1a47548c724b0381cb003804
Juan Sebastian Rodriguez Gomez <j.rodriguezg@uniandes.edu.co>
2025-11-02 17:04:00 -0500

    feature: Creacion de test para album

--- app/src/androidTest/java/com/example/appvinilos/AlbumFragmentTest.kt
+++ app/src/androidTest/java/com/example/appvinilos/AlbumFragmentTest.kt
@@ -1,10 +1,27 @@
 
 package com.example.appvinilos
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4::class)
 class AlbumFragmentTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun test_isAlbumListVisible() {
+        onView(withId(R.id.recyclerAlbums)).check(matches(isDisplayed()))
+        }
+
 
 }
