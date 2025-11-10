61edce9398db03c9171a8a98183f4921b62aa33f
dbeltranandes <d.beltran@uniandes.edu.co>
2025-11-09 20:40:42 -0500

    Avance Sprint 2 - HU03 & HU04 (#78)
    * apk descargada
    
    * funcionalidad y UI de la feature funcionando
    
    * UI y funcionalidad listas
    
    ---------
    
    Co-authored-by: vcamachoandes <v.camacho@uniandes.edu.co>

--- app/src/main/java/com/example/appvinilos/models/Performer.kt
+++ app/src/main/java/com/example/appvinilos/models/Performer.kt
@@ -1,8 +1,17 @@
 package com.example.appvinilos.models
 
+import com.example.appvinilos.models.Album
+import java.util.Date
+
 data class Performer(
     val id: Int,
     val name: String,
     val image: String,
-    val description: String
+    val description: String,
+    // Estos campos son para el detalle y pueden ser nulos
+    val birthDate: Date? = null,
+    val creationDate: Date? = null,
+    val albums: List<Album>? = null,
+    // Campo local para saber el tipo de artista
+    var artistType: String? = null
 )
